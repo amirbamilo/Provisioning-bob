17462fb422cde8288a921c7cbb0057007ebf9764
Amir <amir.jahanshahi@bamilo.com>
2016-05-30 11:00:02 +0430

    Test

---
+++ bob/templates/log.php
@@ -0,0 +1,13 @@
+<?php
+
+/*
+ * Configuration for Bob_Log
+ *
+ * This file is needed, because logging must work !before! the bootstrapping
+ * process.
+ *
+ */
+return array(
+    'log_to_file' => true,
+    'log_internal_communication' => true,
+);
